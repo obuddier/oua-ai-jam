fc11c28636c1f032f2514d0b571e0bc712c779d4
Emirhan <litfinemirhan@gmail.com>
2024-05-19 00:30:53 +0300

    AI(Navmesh) integrated

---
+++ ai-jam/Assets/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Transform target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+        
+        if (Vector3.Distance(transform.position, target.position) < 10.0f)
+        {
+            agent.speed = 6f;
+        }
+        else
+        {
+            agent.speed = 8f;
+        }
+        
+        if(agent.velocity.magnitude > 0.5f)
+        {
+            this.gameObject.GetComponent<Animator>().SetBool("isRunning", true);
+        }
+        else
+        {
+            this.gameObject.GetComponent<Animator>().SetBool("isRunning", false);
+        }
+        
+        if (Vector3.Distance(transform.position, target.position) < 1.5f)
+        {
+            Debug.Log("Game Over");
+            agent.speed = 0f;
+            agent.velocity = Vector3.zero;
+            agent.isStopped = true;
+            agent.ResetPath();
+        }
+    }
+}
